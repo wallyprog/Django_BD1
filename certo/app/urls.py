b62613100d30eb0cffa925ebc379417335e157df
wallyson <wallyprog>
2019-08-30 16:38:59 -0300

    create

--- certo/app/urls.py
+++ certo/app/urls.py
@@ -1,2 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
+from django.conf.urls import url
+
+urlpatterns = [
+    path('marca_form',criar_marca, name='criar_marca'),
+]
+
--- certo/app/views.py
+++ certo/app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_list_or_404
+from django.forms import ModelForm
+from .models import *
 
-# Create your views here.
+class MarcaForm(ModelForm):
+    class Meta:
+        model = Marca
+        fields=['nome','categoria']
+
+
+def criar_marca(request, template_name='marca_form.html'):
+    form = MarcaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('criar_marca')
+    return render(request, template_name,{'form':form})
+
+
+
+
