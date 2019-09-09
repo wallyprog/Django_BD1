5aea75d1ba941b20f4e1fa0332d1e99551c15ff0
wallyson <wallyprog>
2019-09-09 19:45:40 -0300

    Há um bug em editar

--- certo/app/urls.py
+++ certo/app/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('marca_form',criar_marca, name='criar_marca'),
+    path('marca_listar', listar_marca,name='marca_list'),
+    path('marca_deletar//(?P<pk>[0-9]+)', deletar_marca, name = 'deletar_marca'),
+    path('marca_editar//(?P<pk>[0-9]+',editar_marca,name = 'editar_marca'),
 ]
 
--- certo/app/views.py
+++ certo/app/views.py
@@ -1,20 +1,47 @@
-from django.shortcuts import render,redirect,get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
 from .models import *
+
 
 class MarcaForm(ModelForm):
     class Meta:
         model = Marca
-        fields=['nome','categoria']
+        fields = ['nome', 'categoria']
 
 
 def criar_marca(request, template_name='marca_form.html'):
     form = MarcaForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('criar_marca')
-    return render(request, template_name,{'form':form})
+        return redirect('marca_list')
+    return render(request, template_name, {'form': form})
 
 
+def listar_marca(request, template_name='marca_list.html'):
+    query = request.GET.get('busca')
+    if query:
+        marca = Marca.objects.filter(nome__icontains=query)
+    else:
+        marca = Marca.objects.all()
+    marcas = {'lista': marca}
+    return render(request, template_name, marcas)
 
 
+def editar_marca(request, pk, template_name='marca_form.html'):
+    marca = get_object_or_404(Marca, pk = pk)
+    form = MarcaForm(request.POST or None, instance=marca)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('marca_list')
+        else:
+            form = MarcaForm(instance=marca)
+    return render(request, template_name, {'form': marca})
+
+
+def deletar_marca(request, pk, template_name="delete_marca.html"):
+    marca = Marca.objects.get(pk=pk)
+    if request.method == 'POST':
+        marca.delete()
+        return redirect('marca_list')
+    return render(request, template_name, {'marca': marca})
